ac9109ca36df57b23cda1dd74c3381a318c084f1
Afsheenjinan <afsheenameen@gmail.com>
2022-04-01 23:52:56 +0400

    Update chat_screen.dart

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sandy/models/message_model.dart';
 import 'package:sandy/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,6 +14,7 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.primary,
       appBar: AppBar(
         title: Text(widget.user.name),
         actions: [
@@ -22,6 +24,26 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
         ],
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: ListView.builder(
+                  itemCount: messages.length,
+                  itemBuilder: (context, index) {
+                    return Text(messages[index].text);
+                  }),
+            ),
+          ),
+        ],
       ),
     );
   }
