4137fefc49797bf2bfd79ddff92e775ece5dc5c0
Afsheenjinan <afsheenameen@gmail.com>
2022-04-02 20:19:11 +0400

    Edited

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,38 +13,108 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).colorScheme.primary,
-      appBar: AppBar(
-        title: Text(widget.user.name),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.more_horiz),
-            onPressed: () {},
-          ),
-        ],
-        elevation: 0,
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30),
-                  topRight: Radius.circular(30),
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        appBar: AppBar(
+          title: Text(widget.user.name),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.more_horiz),
+              onPressed: () {},
+            ),
+          ],
+          elevation: 0,
+        ),
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            children: [
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: ListView.builder(
+                        reverse: true,
+                        padding: const EdgeInsets.only(top: 20),
+                        itemCount: messages.length,
+                        itemBuilder: (context, index) {
+                          Message message = messages[index];
+                          bool isMe = message.sender.id == currentUser.id;
+                          return _buildMessage(message, isMe);
+                        }),
+                  ),
                 ),
               ),
-              child: ListView.builder(
-                  itemCount: messages.length,
-                  itemBuilder: (context, index) {
-                    return Text(messages[index].text);
-                  }),
-            ),
+              _buildMessageComposer()
+            ],
           ),
-        ],
+        ),
       ),
     );
+  }
+
+  Widget _buildMessage(Message message, bool isMe) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          color: isMe
+              ? Theme.of(context).colorScheme.secondary
+              : const Color(0xFFFFEFEE),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 0,
+              blurRadius: 10,
+              offset: const Offset(2, 2),
+            )
+          ],
+        ),
+        margin: isMe
+            ? const EdgeInsets.only(top: 10, bottom: 10, left: 80, right: 10)
+            : const EdgeInsets.only(top: 10, bottom: 10, left: 10, right: 80),
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(message.text),
+            Text(message.date),
+          ],
+        ));
+  }
+
+  _buildMessageComposer() {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        height: 50,
+        color: Colors.amber.shade50,
+        child: Row(
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.photo),
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            Expanded(
+              child: TextField(
+                onChanged: (value) {},
+                textCapitalization: TextCapitalization.sentences,
+                decoration:
+                    const InputDecoration.collapsed(hintText: "Send a Message"),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.send_rounded),
+              color: Theme.of(context).colorScheme.primary,
+            )
+          ],
+        ));
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,19 +11,6 @@ class HomesScreen extends StatefulWidget {
 }
 
 class _HomesScreenState extends State<HomesScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -106,11 +93,6 @@ class _HomesScreenState extends State<HomesScreen> {
             ),
           ],
         )),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: 'Increment',
-          child: const Icon(Icons.add),
-        ),
       ),
     );
   }
