a0b1551ee7d7cf669f74824b74caa106518d3332
Afsheenjinan <afsheenameen@gmail.com>
2022-04-09 21:33:16 +0400

    Update category_selector.dart

--- lib/widgets/category_selector.dart
+++ lib/widgets/category_selector.dart
@@ -13,29 +13,30 @@ class _CategorySelectorState extends State<CategorySelector> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 60,
-        color: Theme.of(context).colorScheme.primary,
-        child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: categories.length,
-            itemBuilder: (BuildContext context, int index) {
-              return GestureDetector(
-                onTap: () => setState(() {
-                  selectedindex = index;
-                }),
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: Text(categories[index],
-                      style: TextStyle(
-                          color: index == selectedindex
-                              ? Colors.white
-                              : Colors.white60,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.2)),
-                ),
-              );
-            }));
+      height: 60,
+      color: Theme.of(context).colorScheme.primary,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () => setState(() {
+              selectedindex = index;
+            }),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Text(categories[index],
+                  style: TextStyle(
+                      color: index == selectedindex
+                          ? Colors.white
+                          : Colors.white60,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1.2)),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
